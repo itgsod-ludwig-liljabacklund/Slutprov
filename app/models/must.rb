e888a8a570105a4705bd1623ad3c51de2e3a7fb6
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-04-16 12:34:07 +0200

    la till average rating och lowest price metod

--- app/models/must.rb
+++ app/models/must.rb
@@ -5,14 +5,22 @@ class Must
   property :name, String, :required => true
   property :volume, Integer, :required => true
   property :price, Integer, :required => true
-  property :krpercl, Integer, :required => true
   property :manufacturer, String, :required => true
   property :year, Integer, :required => true
   property :img, Text
 
   has n, :recension
+  has n, :affar, :through => :affarmusts
 
   def get_reviews
     return Recension.all(must: self)
+  end
+
+  def average_rating
+    return ((Recension.all(must: self)).avg(:rating)).round(2)
+  end
+
+  def lowest_price
+    return AffarMust.min(:price, :must_id => self.id)
   end
 end
