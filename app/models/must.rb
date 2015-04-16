6ec48234327471932f58a31b05f0f10261ee1017
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-04-16 11:32:06 +0200

    la till recension association och get_reviews metod

--- app/models/must.rb
+++ app/models/must.rb
@@ -8,4 +8,11 @@ class Must
   property :krpercl, Integer, :required => true
   property :manufacturer, String, :required => true
   property :year, Integer, :required => true
+  property :img, Text
+
+  has n, :recension
+
+  def get_reviews
+    return Recension.all(must: self)
+  end
 end
