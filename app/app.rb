c6cfc9b8a0386773c1e08518becfa04af163e990
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-04-16 11:26:43 +0200

    la till must route

--- app/app.rb
+++ app/app.rb
@@ -1,13 +1,20 @@
 class App < Sinatra::Base
+  require 'pp'
   enable :sessions
 
   get '/' do
+    @popmuster = Must.all
     slim :index
   end
 
   get '/muster' do
     @muster = Must.all
     slim :muster
+  end
+
+  get '/must/:id' do |id|
+    @must = Must.first(id: id)
+    slim :must
   end
 
 end
