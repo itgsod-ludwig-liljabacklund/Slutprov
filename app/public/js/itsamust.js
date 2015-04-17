8db5aaad36253579bf53d1ec7647f6fd3d0c1b58
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-04-17 10:39:49 +0200

    fixade hide/show js för recension

--- app/public/js/itsamust.js
+++ app/public/js/itsamust.js
@@ -1,13 +1,19 @@
+var visible = false
+
 function toggleReview() {
     if(visible){
-        document.getElementById("reportname").style.visibility="hidden";
-        document.getElementById("reportdescription").style.visibility="hidden";
-        document.getElementById("submit").style.visibility="hidden";
+        document.getElementById("rating").style.display="none";
+        document.getElementById("reviewcontent").style.display="none";
+        document.getElementById("submitreview").style.display="none";
+        document.getElementById("ratingtext").style.display="none";
+        document.getElementById("reviewform").style.display="none";
         visible = false
     } else {
-        document.getElementById("reportname").style.visibility="visible";
-        document.getElementById("reportdescription").style.visibility="visible";
-        document.getElementById("submit").style.visibility="visible";
+        document.getElementById("rating").style.display="block";
+        document.getElementById("reviewcontent").style.display="block";
+        document.getElementById("submitreview").style.display="block";
+        document.getElementById("ratingtext").style.display="block";
+        document.getElementById("reviewform").style.display="block";
         visible = true
     }
     console.log(visible)
