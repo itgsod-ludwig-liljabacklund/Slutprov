34a2240dcef9941207cd984855bf0c84fd87a433
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-05-11 10:06:47 +0200

    added tests for popular muster

---
+++ app/test/acceptance/popmuster_spec.rb
@@ -0,0 +1,17 @@
+# require_relative 'acceptance_helper'
+# require_relative '../../db/seed'
+#
+# describe('popular muster', :type => :feature) do
+#
+#   before do
+#     DataMapper.auto_migrate!
+#     Seeder.seed!
+#     visit '/'
+#   end
+#
+#   it 'shows the highly rated muster' do
+#     expect(page).to have_content 'Påskmust'
+#     expect(page).to have_no_content 'Julmust'
+#   end
+#
+# end
